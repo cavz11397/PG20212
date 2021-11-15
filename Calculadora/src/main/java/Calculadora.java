d2974caf6d96c55f73fc416853748edb701f8546
Alejo <cavz11397@gmail.com>
2021-11-15 07:35:12 -0500

    [UPDATE] Creacion de la estructura basica

---
+++ Calculadora/src/main/java/Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+
+    public Double suma(Double num1, Double num2) {
+        return num1 + num2;
+    }
+
+    public Double resta(Double num1, Double num2) {
+        return num1 - num2;
+    }
+
+    public Double multiplicacion(Double num1, Double num2) {
+        return num1 * num2;
+    }
+
+    public Double division(Double num1, Double num2) {
+        return num1 / num2;
+    }
+
+}
---
+++ Calculadora/src/test/java/CalculadoraTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    Calculadora calculadora = new Calculadora();
+
+    @Test
+    void suma() {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    @Test
+    void resta() {
+    }
+
+    @Test
+    void multiplicacion() {
+    }
+
+    @Test
+    void division() {
+    }
+}
